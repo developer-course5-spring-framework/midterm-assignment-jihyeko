c614512ab0670be8c251c8385a78539ea11e8fee
kokojihye <jihyevivianako@gmail.com>
2023-06-17 20:18:03 +0900

    Initial commit

---
+++ src/test/java/com/ohgiraffers/midtermassignmentjihyeko/MidtermAssignmentJihyekoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.midtermassignmentjihyeko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidtermAssignmentJihyekoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
